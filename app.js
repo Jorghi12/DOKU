d6c322ebdda935adc339536bcc6f641ec329d823
= <jd728@cornell.edu>
2016-10-28 02:11:38 -0400

    DS

--- app.js
+++ app.js
@@ -205,6 +205,11 @@ app.get('/auth/instagram/callback', passport.authenticate('instagram', { failure
 });
 app.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email', 'user_location'] }));
 app.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/login' }), (req, res) => {
+	if (req.session.returnTo == "/marketplace/loadmore"){
+		res.redirect('/');
+	}
+	console.log("GAINS BRO");
+	console.log(req.session.returnTo);
   res.redirect(req.session.returnTo || '/');
 });
 app.get('/auth/github', passport.authenticate('github'));
